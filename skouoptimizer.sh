76d8c242d08d55730b515d8905bd5c56b54fbd67
Carlos <oditorrinco222@gmail.com>
2025-02-04 15:49:32 -0300

    added other functions, fstrim,timer,ananicy_cpp, initframs

--- skouoptimizer.sh
+++ skouoptimizer.sh
@@ -146,6 +146,125 @@ verificar_e_perguntar_initramfs_lz4() {
     fi
 }
 verificar_e_perguntar_initramfs_lz4 
+verificar_e_perguntar_systemd_initramfs() {
+    if grep -q '^HOOKS=(.*systemd.*)' /etc/mkinitcpio.conf; then
+        echo "O Systemd já está configurado no initramfs. Nenhuma alteração necessária."
+        return
+    fi
 
+    read -p "O Systemd não está configurado no initramfs. Deseja configurar agora? (s/n) " resposta
+    if [[ "$resposta" =~ ^[Ss]$ ]]; then
+        echo "Configurando o mkinitcpio para usar o Systemd..."
 
+        # Substitui HOOKS pelo novo valor
+        sudo sed -i 's/^HOOKS=.*/HOOKS=(systemd autodetect microcode modconf kms keyboard sd-vconsole block filesystems fsck)/' /etc/mkinitcpio.conf
+
+        echo "Atualizando initramfs..."
+        sudo mkinitcpio -P
+
+        echo "Configuração concluída!"
+    else
+        echo "Configuração não aplicada."
+    fi
+}
+verificar_e_perguntar_systemd_initramfs 
+ativar_systemd_oomd() {
+    # Verifica se o systemd-oomd já está ativo
+    if systemctl is-active --quiet systemd-oomd; then
+        echo "systemd-oomd já está ativo."
+        return
+    fi
+
+    # Pergunta se o usuário deseja ativar o systemd-oomd
+    read -p "Deseja ativar o systemd-oomd? (s/n) " resposta
+    if [[ "$resposta" =~ ^[Ss]$ ]]; then
+        sudo systemctl enable --now systemd-oomd
+        echo "systemd-oomd ativado com sucesso."
+    else
+        echo "Operação cancelada."
+    fi
+}
+ativar_systemd_oomd 
+instalar_ananicy_cpp() {
+    # Verifica se o ananicy-cpp está instalado
+    if pacman -Qs ananicy-cpp > /dev/null; then
+        echo "ananicy-cpp já está instalado."
+    else
+        # Pergunta ao usuário se deseja instalar o ananicy-cpp
+        read -p "Deseja instalar o ananicy-cpp? (s/n) " resposta
+        if [[ "$resposta" =~ ^[Ss]$ ]]; then
+            sudo pacman -S ananicy-cpp
+            echo "ananicy-cpp instalado com sucesso."
+        else
+            echo "Operação cancelada."
+            return
+        fi
+    fi
+
+    # Verifica se o serviço ananicy-cpp está ativo
+    if systemctl is-active --quiet ananicy-cpp; then
+        echo "O serviço ananicy-cpp já está ativo."
+    else
+        # Pergunta ao usuário se deseja ativar o serviço
+        read -p "Deseja ativar o serviço ananicy-cpp? (s/n) " resposta
+        if [[ "$resposta" =~ ^[Ss]$ ]]; then
+            sudo systemctl enable --now ananicy-cpp
+            echo "O serviço ananicy-cpp foi ativado com sucesso."
+        else
+            echo "Operação cancelada."
+        fi
+    fi
+
+    # Instalando as regras adicionais
+    read -p "Deseja instalar as regras adicionais do ananicy-cpp? (s/n) " resposta
+    if [[ "$resposta" =~ ^[Ss]$ ]]; then
+        git clone https://aur.archlinux.org/cachyos-ananicy-rules-git.git
+        cd cachyos-ananicy-rules-git
+        makepkg -sric
+        sudo systemctl restart ananicy-cpp
+        echo "Regras adicionais do ananicy-cpp instaladas e serviço reiniciado."
+    else
+        echo "Operação cancelada."
+    fi
+}
+instalar_ananicy_cpp
+verificar_e_configurar_aparar() {
+    # Verificar se o sistema de arquivos BTRFS NÃO está em uso
+    if lsblk -f | grep -q 'btrfs'; then
+        echo "Sistema de arquivos BTRFS detectado. Não será necessário executar o TRIM manualmente, pois o kernel 6.2 ou superior gerencia automaticamente."
+        return
+    fi
+    
+    # Verificar a versão do kernel
+    kernel_version=$(uname -r | cut -d'.' -f1,2)
+    
+    if [[ "$kernel_version" < "6.2" ]]; then
+        # Sistema de arquivos não BTRFS e kernel 6.2 ou inferior
+        echo "Sistema de arquivos não BTRFS ou kernel inferior a 6.2. Continuando com a configuração do fstrim."
+        
+        # Verificar se o fstrim.timer já está em execução
+        if systemctl is-active --quiet fstrim.timer; then
+            echo "fstrim.timer já está em execução. Nenhuma ação necessária."
+            return
+        fi
+        
+        # Pergunta se o usuário deseja habilitar o fstrim.timer ou executar manualmente
+        read -p "Deseja habilitar o fstrim.timer para limpeza automática semanal do SSD? (s/n) " resposta
+        if [[ "$resposta" =~ ^[Ss]$ ]]; then
+            sudo systemctl enable --now fstrim.timer
+            echo "fstrim.timer habilitado para execução automática semanal."
+        else
+            read -p "Deseja executar o comando fstrim agora? (s/n) " resposta
+            if [[ "$resposta" =~ ^[Ss]$ ]]; then
+                sudo fstrim -v /
+                echo "fstrim executado manualmente."
+            else
+                echo "Operação cancelada."
+            fi
+        fi
+    else
+        echo "Kernel 6.2 ou superior detectado. TRIM é gerenciado automaticamente pelo kernel."
+    fi
+}
+verificar_e_configurar_aparar
 
