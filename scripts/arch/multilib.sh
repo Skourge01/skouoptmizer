3e7686ff15b4aadb67ca604bf6eb17e9c8401893
Skourge01 <oditorrinco222@gmail.com>
2025-02-16 09:46:47 -0300

    removed bug in multilib to update system before to activate script

--- scripts/arch/multilib.sh
+++ scripts/arch/multilib.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 ativar_multilib() {
-    sudo sed -i '/^\[multilib\]/,/Include/s/^#//' /etc/pacman.conf
+    sudo sed -i '/^\s*#\s*\[multilib\]/,/\s*#\s*Include/s/^#//' /etc/pacman.conf
     echo "Repositório multilib ativado com sucesso!"
 }
+
 multilib_esta_ativo() {
-    grep -q '^\[multilib\]' /etc/pacman.conf && grep -q '^Include = /etc/pacman.d/mirrorlist' /etc/pacman.conf
+    grep -q '^[^#]*\[multilib\]' /etc/pacman.conf && grep -q '^[^#]*Include = /etc/pacman.d/mirrorlist' /etc/pacman.conf
 }
+
 verificar_e_ativar_multilib() {
     if multilib_esta_ativo; then
         echo "O repositório multilib já está ativado."
@@ -19,4 +21,6 @@ verificar_e_ativar_multilib() {
         echo "Repositório multilib não foi ativado."
     fi
 }
+
 verificar_e_ativar_multilib
+sudo pacman -Syu
